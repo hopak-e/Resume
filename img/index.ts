60945634d10a35a8329689d21048587788435147
hopak-e <a328028@gmail.com>
2024-04-23 13:03:22 +0900

    Updates

---
+++ img/index.ts
@@ -0,0 +1,29 @@
+import MapleInfoCube from "./mapleinfo_cube.gif";
+import MapleInfoGuild from "./mapleinfo_guild.gif";
+import MapleInfoMain from "./mapleinfo_main.gif";
+import MapleInfoStarForce from "./mapleinfo_starforce.gif";
+import MapleInfoTrackChar from "./mapleinfo_track_char.gif";
+import MapleInfoUserData from "./mapleinfo_user_data.gif";
+import ParkSangHo from "./park_sang_ho.jpg";
+import SoopoolBulletinBoard from "./soopool_bulletin_board.gif";
+import SoopoolEditPost from "./soopool_edit_post.gif";
+import SoopoolMyPage from "./soopool_my_page.gif";
+import SoopoolSignIn from "./soopool_sign_in.gif";
+import SoopoolViewPost from "./soopool_view_post.gif";
+import SoopoolWritePost from "./soopool_write_post.gif";
+
+export {
+  MapleInfoCube,
+  MapleInfoGuild,
+  MapleInfoMain,
+  MapleInfoStarForce,
+  MapleInfoTrackChar,
+  MapleInfoUserData,
+  ParkSangHo,
+  SoopoolBulletinBoard,
+  SoopoolEditPost,
+  SoopoolMyPage,
+  SoopoolSignIn,
+  SoopoolViewPost,
+  SoopoolWritePost,
+};
